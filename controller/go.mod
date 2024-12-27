67a4ada092a85ee025cf82d4e30162c22a267020
Francesco Torchia <torchia.francesco50@gmail.com>
2024-12-27 15:00:58 +0100

    Add controller Docker and implementation
    Signed-off-by: Francesco Torchia <torchia.francesco50@gmail.com>
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

---
+++ controller/go.mod
@@ -0,0 +1,5 @@
+module github.com/torchiaf/Sensors/controller
+
+go 1.22.2
+
+require github.com/rabbitmq/amqp091-go v1.10.0
---
+++ controller/rpc_client.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func exec() (res int, err error) {
+
+	host := os.Getenv("RABBITMQ_CLUSTER_SERVICE_HOST")
+	port := os.Getenv("RABBITMQ_CLUSTER_SERVICE_PORT")
+
+	username := os.Getenv("RMQ_USERNAME")
+	password := os.Getenv("RMQ_PASSWORD")
+	// monitorRoutingKey := os.Getenv("ROUTE_1")
+
+	address := fmt.Sprintf("amqp://%s:%s@%s:%s/", username, password, host, port)
+
+	conn, err := amqp.Dial(address)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // noWait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	corrId := randomString(32)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = ch.PublishWithContext(ctx,
+		"",          // exchange
+		"rpc_queue", // routing key
+		false,       // mandatory
+		false,       // immediate
+		amqp.Publishing{
+			ContentType:   "text/plain",
+			CorrelationId: corrId,
+			ReplyTo:       q.Name,
+			Body:          []byte(strconv.Itoa(1)),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	for d := range msgs {
+		if corrId == d.CorrelationId {
+			res, err = strconv.Atoi(string(d.Body))
+			failOnError(err, "Failed to convert body to integer")
+			break
+		}
+	}
+
+	return
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	// n := bodyFrom(os.Args)
+
+	log.Printf(" [x] Requesting")
+	res, err := exec()
+	failOnError(err, "Failed to handle RPC request")
+
+	log.Printf(" [.] Got %d", res)
+}
+
+func bodyFrom(args []string) int {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "1"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	n, err := strconv.Atoi(s)
+	failOnError(err, "Failed to convert arg to integer")
+	return n
+}
