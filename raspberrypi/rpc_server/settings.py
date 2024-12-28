4a45228fab63fff60660e7a59dd87eab9801d19d
Francesco Torchia <torchia.francesco50@gmail.com>
2024-12-28 13:39:22 +0100

    Fix raspberrypi init print
    Signed-off-by: Francesco Torchia <torchia.francesco50@gmail.com>
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- raspberrypi/rpc_server/settings.py
+++ raspberrypi/rpc_server/settings.py
@@ -1,6 +1,12 @@
 import os
+import builtins
 import pika
 
+# Redefine print do show output when running in containers
+def print(*args):
+    builtins.print(*args, sep=' ', end='\n', file=None, flush=True)
+
+# RabbitMQ configs
 host = os.environ.get('RABBITMQ_CLUSTER_SERVICE_HOST')
 port = os.environ.get('RABBITMQ_CLUSTER_SERVICE_PORT_AMQP')
 username = os.environ.get('RABBITMQ_USERNAME')
