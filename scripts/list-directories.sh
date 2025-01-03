61a0cc896d9385eefa825e2308f7beaf1032c4ef
Francesco Torchia <francesco.torchia@suse.com>
2025-01-03 15:39:33 +0100

    Generate modules Dockerfile

--- scripts/list-directories.sh
+++ scripts/list-directories.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-DIRECTORY=$1
-OUTPUT=$2
-
+ROOT=$1
 RET=""
 
-for dir in $(ls $DIRECTORY); do
-    RET=$(echo -n "$RET\"$dir\",")
+for dir in $(ls $ROOT); do
+    RET=$(echo -n "$RET$dir,")
 done
 
-echo -n "$OUTPUT=["
-echo -n $RET | sed 's/.$//'
-echo "]"
+echo $RET
