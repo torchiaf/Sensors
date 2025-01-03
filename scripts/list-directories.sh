4b7b8cc78d3ad73f631125ad29708dacda492c3c
Francesco Torchia <francesco.torchia@suse.com>
2025-01-03 18:34:46 +0100

    Generate modules list in GitHub workflow
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- scripts/list-directories.sh
+++ scripts/list-directories.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
 ROOT=$1
+FORMAT=$2
+REGEX=$3
+
 RET=""
 
 for dir in $(ls $ROOT); do
-    RET=$(echo -n "$RET$dir,")
+    if [ -z "$REGEX" -o $(echo "$dir" | grep "$REGEX") ] ; then
+        if [ "$FORMAT" = "json" ] ; then
+            RET=$(echo -n "$RET\"$dir\",")
+        elif [ "$FORMAT" = "csv" ] ; then
+            RET=$(echo -n "$RET$dir,")
+        fi
+    fi
 done
 
-echo $RET
+if [ "$FORMAT" = "json" ] ; then
+    echo -n "["
+    echo -n $RET | sed 's/.$//'
+    echo "]"
+elif [ "$FORMAT" = "csv" ] ; then
+    echo $RET
+fi
